f5f46884fc9bcbbde1e1792c058ebb70deb124ff
Илья Гусев <mail@igusev.ru>
2015-07-09 20:15:19 +0300

    added command devanswer

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 
-require_once('vendor/autoload.php');
-
+require_once "vendor/autoload.php";
 
 try {
     $json = json_decode(file_get_contents("php://input"), true);
@@ -9,10 +8,14 @@ try {
 
     $incomingMessage = \TelegramBot\Api\Types\Message::fromResponse($json['message']);
 
-    if($incomingMessage->getText() === '/ping') {
-        $bot->sendMessage($incomingMessage->getChat()->getId(), 'pong!');
+    if (in_array($incomingMessage->getText(), ['/devanswer'])) {
+        preg_match_all('/{"text":"(.*?)",/s', file_get_contents('http://devanswers.ru/'), $result);
+
+        $bot->sendMessage($incomingMessage->getChat()->getId(),
+            str_replace("<br/>", "\n", json_decode('"' . $result[1][0] . '"')));
     }
 
 } catch(\TelegramBot\Api\Exception $e) {
     echo $e->getMessage();
 }
+
